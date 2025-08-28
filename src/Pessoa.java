c78cfe9a9c787a2610aca798a1850d31d86efbe1
cristofenatanael@gmail.com <cristofenatanael@gmail.com>
2025-08-28 08:24:12 -0300

    primeiro commit

---
+++ src/Pessoa.java
@@ -0,0 +1,31 @@
+public class Pessoa {
+    private int id;
+    private int idade;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
